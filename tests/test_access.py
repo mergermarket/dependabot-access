6769628748d0989847b7cb010fd67c91535b10b4
Arthur Gassner <arthur.gassner@gmail.com>
2018-12-06 11:32:35 +0000

    Add more tests

--- tests/test_access.py
+++ tests/test_access.py
@@ -85,13 +85,91 @@ class TestDependabotConfiguration(unittest.TestCase):
 
     def setUp(self):
         self._app_id = '12345678'
+        self.org_name = 'fake_org'
 
-    def test_app_get_github_team(self):
-        
+    @patch('dependabot_access.access.Github')
+    def test_app_get_github_team(self, github):
+        # given
         main_team_name = 'test-team'
 
+        mock_team = Mock()
+        mock_team.name = main_team_name
+
+        mock_org = Mock()
+        mock_org.get_teams.return_value = [mock_team]
+        github.return_value.get_organization.return_value = mock_org
+
+        # when
         app = App(ANY, ANY, ANY, self._app_id, ANY)
-        
+        team = app.get_github_team(main_team_name)
+
+        # then
+        assert team
+        assert team.name == main_team_name
+
+    @patch('dependabot_access.access.App.handle_repo')
+    @patch('dependabot_access.access.App.get_github_team')
+    @patch('dependabot_access.access.DependabotRepo')
+    def test_configure_repo_archived(
+        self, dependabot_repo, get_github_team, handle_repo
+    ):
+        repo_name = 'mock-archived-repo-name'
+        #  given
+        repo_config = {
+            'teams': {'team-c': 'pull'},
+            'apps': {'dependabot': True}
+        }
+
+        access_config = {
+            repo_name: repo_config
+        }
+
+        repo_mock = Mock()
+        repo_mock.name = repo_name
+        repo_mock.archived = True
+        repo_mock.permissions.admin = True
+        mock_team = Mock()
+        mock_team.get_repos.return_value = [repo_mock]
+        get_github_team.return_value = mock_team
+
+        # when
+        app = App(ANY, ANY, ANY, self._app_id, ANY)
+        app.configure(access_config)
+
+        # then
+        app.handle_repo.assert_not_called()
+
+    @patch('dependabot_access.access.App.handle_repo')
+    @patch('dependabot_access.access.App.get_github_team')
+    @patch('dependabot_access.access.DependabotRepo')
+    def test_configure_repo_non_admin(
+        self, dependabot_repo, get_github_team, handle_repo
+    ):
+        repo_name = 'mock-non_admin-repo-name'
+        #  given
+        repo_config = {
+            'teams': {'team-c': 'pull'},
+            'apps': {'dependabot': True}
+        }
+
+        access_config = {
+            repo_name: repo_config
+        }
+
+        repo_mock = Mock()
+        repo_mock.name = repo_name
+        repo_mock.archived = False
+        repo_mock.permissions.admin = False
+        mock_team = Mock()
+        mock_team.get_repos.return_value = [repo_mock]
+        get_github_team.return_value = mock_team
+
+        # when
+        app = App(ANY, ANY, ANY, self._app_id, ANY)
+        app.configure(access_config)
+
+        # then
+        app.handle_repo.assert_not_called()
 
     @patch('dependabot_access.access.App.handle_repo')
     @patch('dependabot_access.access.App.get_github_team')
@@ -147,6 +225,27 @@ class TestDependabotConfiguration(unittest.TestCase):
 
         # then
         app.enforce_app_access.assert_called_once_with(repo_mock, app_config)
+
+    @patch('dependabot_access.access.App.enforce_app_access')
+    @patch('dependabot_access.access.App.install_app_on_repo')
+    @patch('dependabot_access.access.DependabotRepo')
+    def test_app_handle_repo_no_config(
+        self, dependabot_repo, install_app_on_repo, enforce_app_access
+    ):
+        #  given
+        mock_error = Mock()
+        mock_repo = Mock()
+        mock_repo.name = 'mock-repo'
+
+        # when
+        app = App(ANY, ANY, ANY, self._app_id, mock_error)
+        app.handle_repo(mock_repo, None)
+
+        # then
+        mock_error.assert_called_once_with(
+            'repository mock-repo has no config'
+        )
+        app.enforce_app_access.assert_not_called()
 
     @patch('dependabot_access.access.App.install_app_on_repo')
     @patch('dependabot_access.access.DependabotRepo')
