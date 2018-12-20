80aa73bb33beab1c5597900a72336b84d6d2a065
Arthur Gassner <arthur.gassner@gmail.com>
2018-12-20 12:31:47 +0000

    WIP refactoring requests to use session

--- dependabot_access/access.py
+++ dependabot_access/access.py
@@ -4,7 +4,7 @@ import logging
 import os
 import requests
 
-from github import Github
+from collections import namedtuple
 from . dependabot import DependabotRepo
 
 logger = logging.getLogger()
@@ -21,37 +21,67 @@ class App():
         self.account_id = account_id
         self.on_error = on_error
 
+        self.github_headers = {
+            'Authorization': f"token {self.github_token}",
+            'Accept': "application/vnd.github.machine-man-preview+json",
+            'Cache-Control': "no-cache",
+        }
+
+        self.github_request_session = requests.Session()
+        self.github_request_session.headers.update(self.github_headers)
+
+        self.dependabot_repo = DependabotRepo(self.account_id, self.on_error)
+
     def configure(self, config_list):
         for config in config_list:
             if config.get('apps', {}).get('dependabot', False):
                 for repo_name in config.get('repos', []):
                     self.enforce_app_access(repo_name)
 
+    def get_repo_contents(self, repo_name):
+        no_repo_contents_status_code = 404
+        response = self.github_request_session.request(
+            'GET',
+            f'https://api.github.com/repos/{self.org_name}/'
+            f'{repo_name}/contents'
+        )
+        if response.status_code == no_repo_contents_status_code:
+            logger.info(f'Repo {repo_name} has no content')
+            return []
+        return response.json()
+
     def enforce_app_access(self, repo_name):
         repo = self.get_github_repo(repo_name)
-        if repo.archived or not repo.permissions.admin:
+        if repo.archived or not repo.admin:
             return
         self.install_app_on_repo(self.app_id, repo)
-        dependabot = DependabotRepo(repo, self.account_id, self.on_error)
-        dependabot.add_configs_to_dependabot()
+        repo_files = self.get_repo_contents(repo_name)
+
+        self.dependabot_repo.add_configs_to_dependabot(repo, repo_files)
 
     def get_github_repo(self, repo_name):
-        github = Github(self.github_token)
         logger.info(f'Getting repo: {repo_name}')
-        return github.get_repo(f'{self.org_name}/{repo_name}')
+        response = self.github_request_session.request(
+            'GET',
+            f'https://api.github.com/repos/{self.org_name}/'
+            f'{repo_name}'
+        )
+        repo_content = response.json()
+        repo = namedtuple('Repository', 'id, name, archived, admin')(
+            repo_content.get('id'),
+            repo_content.get('name'),
+            repo_content.get('archived'),
+            repo_content.get('permissions').get('admin')
+        )
+        return repo
 
     def install_app_on_repo(self, app_id, repo):
         url = (
             f'https://api.github.com/user/installations/{app_id}/'
             f'repositories/{repo.id}'
         )
-        headers = {
-            'Authorization': f"token {self.github_token}",
-            'Accept': "application/vnd.github.machine-man-preview+json",
-            'Cache-Control': "no-cache",
-        }
         logger.info(f'Installing app on {repo.name} in Github')
-        response = requests.request("PUT", url, headers=headers)
+        response = self.github_request_session.request("PUT", url)
         if response.status_code != 204:
             self.on_error(
                 f'Failed to add repo {repo.name} to Dependabot'
